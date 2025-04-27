f2f26bb1af97d666e20842cb9003440e46dd86b5
itsdevopsprofile <146256531+itsdevopsprofile@users.noreply.github.com>
2025-04-27 17:55:53 +0530

    Create ec2vars.tf

---
+++ Terraform/Simple_Modules/Modules/ec2/ec2vars.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+   type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "pri_subnet_id" {
+  type = string
+}
+
+variable "pub_subnet_id" {
+  type = string
+  
+}
+
+variable "security_group_ids" {
+  type = string
+}
