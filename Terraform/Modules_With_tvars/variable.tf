25ff2cf1e6d7c849c25288d91e5482d8aa490717
itsdevopsprofile <146256531+itsdevopsprofile@users.noreply.github.com>
2025-04-27 18:39:04 +0530

    Update variable.tf

--- Terraform/Modules_With_tvars/variable.tf
+++ Terraform/Modules_With_tvars/variable.tf
@@ -31,15 +31,3 @@ variable "instance_type" {
   type = string
 }
 
-variable "pri_subnet_id" {
-  type = string
-}
-
-variable "pub_subnet_id" {
-  type = string
-  
-}
-
-variable "security_group_ids" {
-  type = string
-}
