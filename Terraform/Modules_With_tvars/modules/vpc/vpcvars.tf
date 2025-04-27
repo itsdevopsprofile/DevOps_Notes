2920ebf5257b2c5537e74c5e1e2afa619a29bbf4
itsdevopsprofile <146256531+itsdevopsprofile@users.noreply.github.com>
2025-04-27 18:20:08 +0530

    Create vpcvars.tf

---
+++ Terraform/Modules_With_tvars/modules/vpc/vpcvars.tf
@@ -0,0 +1,25 @@
+variable "vpc_cidr_block" {
+    type = string 
+    
+}
+
+variable "subnet_cidr_block" {
+    type = list(string)
+  
+}
+
+variable "az" {
+    type = list(string)
+    
+}
+
+variable "public_ip" {
+    type = bool  
+}
+
+variable "no_ip" {
+    type = bool
+  
+}
+
+
