c11177ab71f342da7ac6600e99ca7418e5cfcdeb
itsdevopsprofile <146256531+itsdevopsprofile@users.noreply.github.com>
2026-02-25 22:15:55 +0530

    Create ec2-variable.tf

---
+++ TERRAFORM/terraform-modules/modules/ec2/ec2-variable.tf
@@ -0,0 +1,25 @@
+variable "ami_id" {
+  type    = string
+
+}
+
+
+variable "ins_type" {
+  type    = string
+  
+}
+
+variable "key" {
+  type    = string
+
+}
+
+
+variable "subnet" {
+  type = string
+}
+
+
+variable "security_group"{
+    type = string 
+}
