4cbb8be47c7664bdb4ecdbb4ac9412e7ed52e9b1
itsdevopsprofile <146256531+itsdevopsprofile@users.noreply.github.com>
2026-03-09 23:01:40 +0530

    Create variable.tf

---
+++ TERRAFORM/modules-with-tfvars/modules/ec2/variable.tf
@@ -0,0 +1,22 @@
+variable "ami_id" {
+ type = string 
+}
+
+variable "instance_type" {
+    type = string
+
+}
+
+
+variable "key" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+
+variable "security_group_id" {
+  type =  string
+}
