d60eed8d852bcced53435095b108368d9bd1f754
itsdevopsprofile <146256531+itsdevopsprofile@users.noreply.github.com>
2026-03-09 22:59:52 +0530

    Create variable.tf

---
+++ TERRAFORM/modules-with-tfvars/modules/vpc/variable.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr_block" {
+    type =  string 
+
+}
+
+variable "subnet_cidr_block" {
+  type = string
+}
+
+variable "az" {
+  type =  string
+}
+
+
+variable "public_ip" {
+  type = bool
+}
